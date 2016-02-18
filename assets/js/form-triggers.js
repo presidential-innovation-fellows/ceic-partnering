10aca4852fb9521e4b1c8149015864cc461432c9
Luke Keller <lukeekeller@gmail.com>
2016-02-18 16:16:17 -0500

    Updates

---
+++ assets/js/form-triggers.js
@@ -0,0 +1,55 @@
+var ceic = ceic || {};
+
+ceic.formTriggers = {
+
+  bindFormTriggers: function() {
+    $(".form-trigger").on("click", function() {
+      event.preventDefault();
+      var formId = "#" + $(this).data("form-id");
+      ceic.formTriggers.hideForms();
+      ceic.formTriggers.showForm(formId);
+      ceic.formTriggers.showOverlay();
+    });
+  },
+
+  bindOverlay: function() {
+    $(".overlay").on("click", function() {
+      ceic.formTriggers.hideForms();
+    });
+  },
+
+  repeatValue: function(showed, hiddenField) {
+    document.getElementById(hiddenField).value = showed.value;
+  },
+
+  hideForms: function() {
+    $(".form-modal").removeClass("active");
+    ceic.formTriggers.hideOverlay();
+  },
+
+  hideOverlay: function() {
+    $(".overlay").removeClass("active");
+  },
+
+  showForm: function(formId) {
+    var $form = $(formId),
+        $formModal = $form.closest(".form-modal");
+    $formModal.addClass("active");
+  },
+
+  showOverlay: function() {
+    $(".overlay").addClass("active");
+    ceic.formTriggers.bindOverlay();
+  },
+
+  validateForms: function() {
+    $("#form1").parsley();
+    $("#form3").parsley();
+    $("#form4").parsley();
+  }
+};
+
+$(document).ready(function(){
+  ceic.formTriggers.validateForms();
+  ceic.formTriggers.bindFormTriggers();
+});
